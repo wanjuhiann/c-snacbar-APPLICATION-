bffed5f2983718bb73252e86b32b9a6904dbd270
wanjuhiann <106369744+wanjuhiann@users.noreply.github.com>
2022-07-15 12:29:14 +0300

    Add files via upload

---
+++ ANN NYAMBURA ASSIGNOSNACKBAR.c
@@ -0,0 +1,46 @@
+/*
+zzetech c snack bar
+created by DIt-01-8862/2021
+On July 14
+C89
+MIT licence
+*/
+
+/#include <stdio.h>
+#include <stdlib.h>
+
+
+int mainc()
+{
+        int Item-[6] = {1,2,3,4,5,6};
+        int Item-2;
+
+
+        printf(welcome: nyambu c snack bar shop\n\");
+
+        printf("TODAYS MENU");
+            Printf("What would you like to order:\n");
+        Printf("Food\n");
+        printf("Hotdog/ kes 90\n");
+        printf("Hamburger/keskes300\n");
+        printf("frencfries/kes150\n");
+        printf("milk shake/kes150\n");
+        printf("coffe/kes70\n");
+        printf("beef smokie/kes40\n");
+        printf("please item number:");
+        printf("please item quantity:");
+        scanf("%d",&a);
+        for(int j=0;j<a;j++)
+        {
+            printf("Enter item code:");
+            scanf("%d",&item item-code2);
+
+        }
+            for(i=0;i<a;i++)
+             {
+                 printf("%d",&item item-code2);
+             }
+
+    return 0;
+}
+''
